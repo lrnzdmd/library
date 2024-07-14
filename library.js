27bd51e6d812006e31d4ba5a208c5ee342980139
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-15 01:29:25 +0200

    can now add a card successfully, onto the removal part

--- library.js
+++ library.js
@@ -1,17 +1,22 @@
 const myLibrary = [];
+let cardGrid;
+let cardTemplate;
 
 document.addEventListener("DOMContentLoaded", function() {
     const addDialog = document.getElementById("addbookdialog");
     const addButton = document.getElementById("addbookbutton");
     const dialogCloseBtn = document.getElementById("cancelbutton");
     const bookForm = document.getElementById("bookform");
+    cardGrid = document.getElementById("cardgrid");
+    cardTemplate = document.getElementById("cardtemplate");
 
     addButton.addEventListener("click", function(){
         addDialog.showModal();
     });
 
-    dialogCloseBtn.addEventListener("click", function(){
-        addDialog.close();
+    dialogCloseBtn.addEventListener("click", function(event){
+        const dialog = event.target.closest("dialog");
+        dialog.close();
     });
     
     // Creates a new book from the form data when submitted
@@ -20,15 +25,30 @@ document.addEventListener("DOMContentLoaded", function() {
         const formData = new FormData(bookForm);
         const data = Object.fromEntries(formData.entries());
         data.isRead = formData.has("isread");
+        const dialog = event.target.closest("dialog");
         const newBook = new Book(data.title, data.author, data.pages, data.isRead)
         addBookToLibrary(myLibrary, newBook);
+        addBookCard(newBook, myLibrary.length-1);
         bookForm.reset();
-        addDialog.close();
+        dialog.close();
     });
+
+
 });
 
 function addBookToLibrary(library, book) {
     library.push(book);
+}
+
+function addBookCard(book, index) {
+    const newCard = cardTemplate.content.cloneNode(true);
+    newCard.querySelector('.title').textContent = book.title;
+    newCard.querySelector('.author').textContent = `Author: ${book.author}`;
+    newCard.querySelector('.pages').textContent = `Pages: ${book.pages}`;
+    newCard.querySelector('.readbutton').textContent = book.isRead ? 'Read: Yes' : 'Read: No';
+    newCard.querySelector('.bookcard').setAttribute('id', `${index}`);
+    cardGrid.appendChild(newCard);
+    console.log("just added book index " + index);
 }
 
 function Book(title, author, pages, read) {
@@ -47,3 +67,5 @@ function Book(title, author, pages, read) {
         this.read = !this.read;
     }
 }
+
+
