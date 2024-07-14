6c52c64003f20d85dae2631bc683baa3fb5d9c5b
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-14 20:35:50 +0200

    form is ready and better looking, now it does not break on mobile

--- library.js
+++ library.js
@@ -20,7 +20,18 @@ let myLibrary = [];
 let hobbit = new Book("hobbit","stocazzo","8818",false);
 
 document.addEventListener("DOMContentLoaded", function() {
-    
+    const addDialog = document.getElementById("addbookdialog");
+    const addButton = document.getElementById("addbookbutton");
+    const dialogCloseBtn = document.getElementById("cancelbutton");
+    const dialogSubmitBtn = document.getElementById("submitbutton");
+
+    addButton.addEventListener("click", function(){
+        addDialog.showModal();
+    });
+
+    dialogCloseBtn.addEventListener("click", function(){
+        addDialog.close();
+    });
 });
 
 function changeContent(string) {
