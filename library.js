2d4163e99dd3c7811523c9091b42fa2e70345b5d
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-17 21:29:28 +0200

    all done, i think, in the future i would like to make that search bar functional

--- library.js
+++ library.js
@@ -4,7 +4,10 @@ class Library {
       this.collection = [];
       this.DisplayManager = new DisplayManager();
       this.addBook = this.addBook.bind(this);
+      this.removeBook = this.removeBook.bind(this);
       this.closeDialog = this.closeDialog.bind(this);
+      this.dBtnToggleRead = this.dBtnToggleRead.bind(this)
+
     }
     
     // Will be used at the beginning to create all the listeners on the page minus the ones that will be created with the cards
@@ -13,7 +16,7 @@ class Library {
       this.DisplayManager.addButton.addEventListener("click", () => this.DisplayManager.addDialog.showModal());
       this.DisplayManager.dialogCloseBtn.forEach(btn => btn.addEventListener("click", this.closeDialog));
       this.DisplayManager.bookForm.addEventListener("submit", this.addBook)
-      //this.DisplayManager.removeConfirmBtn.addEventListener("click", removeBook)
+      this.DisplayManager.removeConfirmBtn.addEventListener("click", this.removeBook)
     }
 
     addBook(event) {
@@ -25,25 +28,55 @@ class Library {
       this.DisplayManager.bookForm.reset();
       this.collection.push(newBook);
       this.closeDialog(event);
+      this.dUpdateDisplay();
     }
     
     // removes the book, still have to change it from console only to the ui,
     // maybe i will change how this whole thing work
 
-    removeBook() {
-      let name = prompt("enter title of book to remove (case sensitve)");
-      const index = this.collection.findIndex((bk) => bk.name === name);
+    removeBook(event) {
+        let title = event.target.closest("dialog").querySelector("b").textContent
+        
+      const index = this.collection.findIndex(bk => bk.name === title);
       this.collection.splice(index, 1);
-    }
-  
-    printCollection() {
-      console.log(this.collection);
+      this.closeDialog(event);
+      this.dUpdateDisplay();
     }
   
     closeDialog(event) {
       const dialog = event.target.closest("dialog");
       dialog.close();
-  }
+    }
+
+    dUpdateDisplay() {
+        this.DisplayManager.cardGrid.innerHTML = "";
+        this.collection.forEach(book => {
+            this.dNewBookCard(book);
+        });
+    }
+
+    dNewBookCard(book) {
+        const newCard = this.DisplayManager.cardTemplate.content.cloneNode(true);
+        newCard.querySelector(".title").textContent = book.title;
+        newCard.querySelector(".author").textContent = book.author;
+        newCard.querySelector(".pages").textContent = book.pages;
+        newCard.querySelector(".readbutton").textContent = book.read ? "Read: Yes" : "Read: No";
+        newCard.querySelector(".readbutton").addEventListener("click", this.dBtnToggleRead);
+        newCard.querySelector(".removebutton").addEventListener("click", () => {
+            this.DisplayManager.removeDialog.querySelector("b").textContent = book.title;
+            this.DisplayManager.removeDialog.showModal();
+        });
+        this.DisplayManager.cardGrid.appendChild(newCard);
+    }
+
+    dBtnToggleRead(event) {
+        const btn = event.target;
+        const bookTitle = btn.closest(".bookcard").querySelector(".title").innerHTML;
+        const bookid = this.collection.findIndex(book => book.title === bookTitle);
+        this.collection[bookid].readSwitch();
+        btn.textContent = this.collection[bookid].read ? "Read: Yes" : "Read: No";
+    }
+    
   }
   
   // all the references to dom elements i might need are bundled up in here
